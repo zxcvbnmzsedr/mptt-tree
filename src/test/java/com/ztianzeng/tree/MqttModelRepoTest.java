91142c247783fb88857010eef74dde3634496d7c
zxcvbnmzsedr <tianzneg@gmail.com>
2019-07-12 17:02:04 +0800

    should_add_child_to_child_to_root

--- src/test/java/com/ztianzeng/tree/MqttModelRepoTest.java
+++ src/test/java/com/ztianzeng/tree/MqttModelRepoTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.UUID;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -27,8 +29,8 @@ public class MqttModelRepoTest {
 
     @Test
     public void should_set_root_when_there_is_none() {
-
-        MpttModel root = new MpttModel("root");
+        String rootS = getRandomRoot();
+        MpttModel root = new MpttModel(rootS);
         repo.addRoot(root);
 
         assertThat(root.getLft(), equalTo(1L));
@@ -39,11 +41,12 @@ public class MqttModelRepoTest {
     @Test
     public void fails_to_set_as_root_when_root_already_exists() {
 
-        MpttModel root = new MpttModel("root");
+        String rootS = getRandomRoot();
+        MpttModel root = new MpttModel(rootS);
         repo.addRoot(root);
 
         try {
-            MpttModel root2 = new MpttModel("root");
+            MpttModel root2 = new MpttModel(rootS);
             repo.addRoot(root2);
         } catch (MqttException mq) {
 
@@ -53,8 +56,9 @@ public class MqttModelRepoTest {
 
     @Test
     public void add_first_child_to_root() {
-        MpttModel root = new MpttModel("root");
-        MpttModel child = new MpttModel("child");
+        String rootS = getRandomRoot();
+        MpttModel root = new MpttModel(rootS);
+        MpttModel child = new MpttModel(rootS);
 
         repo.addRoot(root);
         repo.addChild(root.getId(), child);
@@ -67,5 +71,37 @@ public class MqttModelRepoTest {
         assertThat(child.getLft(), equalTo(2L));
         assertThat(child.getRgt(), equalTo(3L));
         assertThat(child.getLevel(), equalTo(1L));
+    }
+
+    @Test
+    public void should_add_child_to_child_to_root() {
+        String rootS = getRandomRoot();
+        MpttModel root = new MpttModel(rootS);
+        MpttModel level1Child = new MpttModel(rootS);
+        MpttModel level2Child = new MpttModel(rootS);
+
+        repo.addRoot(root);
+        repo.addChild(root.getId(), level1Child);
+        repo.addChild(level1Child.getId(), level2Child);
+
+        root = repo.get(root.getId());
+        assertThat(root.getLft(), equalTo(1L));
+        assertThat(root.getRgt(), equalTo(6L));
+        assertThat(root.getLevel(), equalTo(0L));
+
+        level1Child = repo.get(level1Child.getId());
+        assertThat(level1Child.getLft(), equalTo(2L));
+        assertThat(level1Child.getRgt(), equalTo(5L));
+        assertThat(level1Child.getLevel(), equalTo(1L));
+
+        level2Child = repo.get(level2Child.getId());
+        assertThat(level2Child.getLft(), equalTo(3L));
+        assertThat(level2Child.getRgt(), equalTo(4L));
+        assertThat(level2Child.getLevel(), equalTo(2L));
+
+    }
+
+    private String getRandomRoot() {
+        return UUID.randomUUID().toString();
     }
 }
